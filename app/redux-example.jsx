3d4fa1ab4202d39246b0e2bdf42e0df6631aec1a
StephenGrable1 <stephengrable@gmail.com>
2016-09-11 15:58:03 -0400

    Add support for CHANGE_SEARCH_TEXT'

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -6,10 +6,26 @@ console.log('starting redux example');
 var reducer = (state = {name: 'Anonymous'}, action) => {
 //  state = state || {name: 'Anonymous'};
 
-return state;
+switch (action.type) {
+  case 'CHANGE_NAME':
+    return {
+      ...state,
+      name: action.name
+    };
+  default:
+   return state;
+ }
 };
 var store = redux.createStore(reducer);
 
 var currentSate = store.getState();
 
 console.log('currentSate', currentSate);
+
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Stephen'
+});
+
+console.log('Name should be Stephen', store.getState());
